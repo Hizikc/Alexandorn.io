f49a9d7c495e0e217b03aa92ceedb11885de9288
hiz <kuznecovmatvej717@gmail.com>
2026-07-20 17:00:18 +0500

    -

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
+  base: './', // ОБЯЗАТЕЛЬНО точка и слэш! Это заставит пути на гитхабе работать правильно
   server: {
     host: '127.0.0.1',
     port: 3000,
