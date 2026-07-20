99e9a20a71df9fe7563fca8c629ef7aa3646b7c5
hiz <kuznecovmatvej717@gmail.com>
2026-07-20 16:56:30 +0500

    -

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
-  // Обязательно возвращаем имя репозитория, иначе пути к файлам на гитхабе отвалятся!
-  base: '/alexandoin.io/',
-
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-    rollupOptions: {
-      // Здесь должны быть ТОЛЬКО HTML файлы страницы
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        notFound: resolve(__dirname, '404.html')
-      }
-    }
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    open: true
   }
 });
