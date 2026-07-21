ba6226333afbb49450e8efb2e514a0b7b04ac3c4
hiz <kuznecovmatvej717@gmail.com>
2026-07-21 19:51:28 +0500

    Убрал частицы с фона

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
-  base: '/', // ОБЯЗАТЕЛЬНО точка и слэш! Это заставит пути на гитхабе работать правильно
+  base: '/',
   server: {
     host: '127.0.0.1',
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'docs', // <-- Указываем Vite собирать сайт прямо в папку docs
+    emptyOutDir: true // Перед сборкой очистит старый хлам в docs
   }
 });
